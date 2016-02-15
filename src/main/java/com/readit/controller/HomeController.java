ab328105af8d920dc9c2c4c75746dfae27b5d2b5
nkharabaruk <nkharabaruk@gmail.com>
2016-02-15 12:37:53 +0200

    Added Spring Security

--- src/main/java/com/readit/controller/HomeController.java
+++ src/main/java/com/readit/controller/HomeController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -42,6 +43,25 @@ public class HomeController {
         model.addObject("bookList", books);
         return model;
     }
+
+    @RequestMapping(value = "/admin**", method = RequestMethod.GET)
+    public ModelAndView adminPage() {
+        ModelAndView model = new ModelAndView();
+        model.addObject("title", "Spring Security Hello World");
+        model.addObject("message", "This is protected page - Admin Page!");
+        model.setViewName("admin");
+        return model;
+    }
+
+    @RequestMapping(value = "/dba**", method = RequestMethod.GET)
+    public ModelAndView dbaPage() {
+        ModelAndView model = new ModelAndView();
+        model.addObject("title", "Spring Security Hello World");
+        model.addObject("message", "This is protected page - Database Page!");
+        model.setViewName("admin");
+        return model;
+    }
+
 
     @RequestMapping("/categories")
     public ModelAndView listCategories() {
