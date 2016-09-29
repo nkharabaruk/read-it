4049244a7eee338f6267164a21487eced34e7da0
nataliia <nkharabaruk@gmail.com>
2016-09-29 20:37:46 +0300

    RI-5:  create Repositories for new Entities

---
+++ repository/src/main/java/com/readit/repository/QuoteRepository.java
@@ -0,0 +1,13 @@
+package com.readit.repository;
+
+import com.readit.entity.Profile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by nataliia on 29.09.16.
+ */
+
+@Repository
+public interface QuoteRepository extends JpaRepository<Profile, Long> {
+}
--- repository/src/test/java/com/readit/repository/FileRepositoryTest.java
+++ repository/src/test/java/com/readit/repository/FileRepositoryTest.java
@@ -24,7 +24,7 @@ import static org.junit.Assert.*;
 public class FileRepositoryTest {
 
     @Autowired
-    private ImageRepository imageRepository;
+    private FileRepository fileRepository;
 
     private File file1;
     private File file2;
@@ -44,84 +44,84 @@ public class FileRepositoryTest {
 
     @Test
     public void saveTest() {
-        imageRepository.save(file1);
-        assertEquals(file1, imageRepository.findOne(file1.getId()));
+        fileRepository.save(file1);
+        assertEquals(file1, fileRepository.findOne(file1.getId()));
     }
 
     @Test
     public void saveIterableTest() {
-        imageRepository.save(files);
-        assertTrue(imageRepository.findAll().containsAll(files));
+        fileRepository.save(files);
+        assertTrue(fileRepository.findAll().containsAll(files));
     }
 
     @Test
     public void existsTest() {
-        imageRepository.save(file1);
-        assertTrue(imageRepository.exists(file1.getId()));
+        fileRepository.save(file1);
+        assertTrue(fileRepository.exists(file1.getId()));
     }
 
     @Test
     public void findOneTest() {
-        imageRepository.save(file1);
-        assertEquals(file1, imageRepository.findOne(file1.getId()));
+        fileRepository.save(file1);
+        assertEquals(file1, fileRepository.findOne(file1.getId()));
     }
 
     @Test
     public void findAllTest() {
-        imageRepository.save(files);
-        assertTrue(imageRepository.findAll().containsAll(files));
+        fileRepository.save(files);
+        assertTrue(fileRepository.findAll().containsAll(files));
     }
 
     @Test
     public void findAllByIdsTest() {
-        imageRepository.save(files);
+        fileRepository.save(files);
         List<Long> ids = files.stream().map(File::getId).collect(Collectors.toList());
-        List<File> foundFiles = imageRepository.findAll(ids);
+        List<File> foundFiles = fileRepository.findAll(ids);
         assertEquals(files, foundFiles);
 
     }
 
     @Test
     public void countTest() {
-        long before = imageRepository.count();
-        imageRepository.save(files);
-        assertEquals(before + files.size(), imageRepository.count());
+        long before = fileRepository.count();
+        fileRepository.save(files);
+        assertEquals(before + files.size(), fileRepository.count());
     }
 
     @Test
     public void deleteTest() {
-        imageRepository.save(file1);
-        assertTrue(imageRepository.exists(file1.getId()));
-        imageRepository.delete(file1);
-        assertFalse(imageRepository.exists(file1.getId()));
+        fileRepository.save(file1);
+        assertTrue(fileRepository.exists(file1.getId()));
+        fileRepository.delete(file1);
+        assertFalse(fileRepository.exists(file1.getId()));
     }
 
     @Test
     public void deleteByIdTest() {
-        imageRepository.save(file1);
-        assertTrue(imageRepository.exists(file1.getId()));
-        imageRepository.delete(file1.getId());
-        assertFalse(imageRepository.exists(file1.getId()));
+        fileRepository.save(file1);
+        assertTrue(fileRepository.exists(file1.getId()));
+        fileRepository.delete(file1.getId());
+        assertFalse(fileRepository.exists(file1.getId()));
     }
 
     @Test
     public void deleteIterableTest() {
-        imageRepository.save(files);
+        fileRepository.save(files);
         for (File file : files) {
-            assertTrue(imageRepository.exists(file.getId()));
+            assertTrue(fileRepository.exists(file.getId()));
         }
-        imageRepository.delete(files);
+        fileRepository.delete(files);
         for (File file : files) {
-            assertFalse(imageRepository.exists(file.getId()));
+            assertFalse(fileRepository.exists(file.getId()));
         }
     }
 
     @Test
     public void deleteAllTest() {
-        imageRepository.save(files);
-        assertNotEquals(0, imageRepository.count());
-        imageRepository.deleteAll();
-        assertEquals(0, imageRepository.count());
+        fileRepository.save(files);
+        assertNotEquals(0, fileRepository.count());
+        fileRepository.deleteAll();
+        assertEquals(0, fileRepository.count());
     }
 
 }
