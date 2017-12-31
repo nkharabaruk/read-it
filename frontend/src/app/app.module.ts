63445fbfa7d599bccc45d17257eee1945a294e47
Andrii Stefanskyi <stefandriy@gmail.com>
2018-01-01 01:31:33 +0200

    add @ng-bootstrap and fix navbar

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import {BrowserModule} from "@angular/platform-browser";
 import {NgModule} from "@angular/core";
 import {FormsModule} from "@angular/forms";
 import {HttpModule} from "@angular/http";
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import {AppComponent} from "./app.component";
 import {HomeComponent} from "./home/home.component";
@@ -22,7 +23,8 @@ import {AuthorService} from "../shared/service/author.service";
     BrowserModule,
     FormsModule,
     HttpModule,
-    RoutingModule
+    RoutingModule,
+    NgbModule.forRoot()
   ],
   providers: [
     AuthorService,
